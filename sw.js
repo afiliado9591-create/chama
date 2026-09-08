92adb104d3660e61f0f15ed91a2a08508eef03a5
afiliado9591-create <afiliado9591@gmail.com>
2026-09-08 12:43:05 -0300

    Força atualização corrigida do login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="chama-clean-v190";
+const VERSION="chama-clean-v191";
 
 self.addEventListener("install", event => {
   self.skipWaiting();
@@ -15,8 +15,8 @@ self.addEventListener("activate", event => {
       try {
         const url = new URL(client.url);
         if (url.origin !== self.location.origin) return;
-        if (url.searchParams.get("chama_update") === "190") return;
-        url.searchParams.set("chama_update", "190");
+        if (url.searchParams.get("chama_update") === "191") return;
+        url.searchParams.set("chama_update", "191");
         await client.navigate(url.toString());
       } catch (_) {}
     }));
